48f1288301df848b2c7366ffc8effb7098b56c18
julianmejiac <julianmejiac@gmail.com>
2026-09-22 17:34:20 -0400

    Add Spring profiles for environment configuration

--- src/main/java/com/julianmejiac/walletoptimizer/controller/CardController.java
+++ src/main/java/com/julianmejiac/walletoptimizer/controller/CardController.java
@@ -10,8 +10,6 @@ import com.julianmejiac.walletoptimizer.service.CardService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.plaf.basic.BasicDesktopIconUI;
 import java.util.List;
 
 @RestController
